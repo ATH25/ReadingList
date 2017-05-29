35c66b055570ce6d59a9fd15c875c3681d3f07d6
AjuThomas <aju_thomas@yahoo.com>
2017-05-29 04:51:43 -0400

    test

--- src/main/java/com/thomas/readinglist/ReadingListApplication.java
+++ src/main/java/com/thomas/readinglist/ReadingListApplication.java
@@ -3,6 +3,8 @@ package com.thomas.readinglist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//comments
+
 @SpringBootApplication
 public class ReadingListApplication {
 
