86a0f27143ec99739c36a0fc70ce2824fe8eb3f8
AjuThomas <aju_thomas@yahoo.com>
2017-05-29 05:03:37 -0400

    test

--- src/main/java/com/thomas/readinglist/Reader.java
+++ src/main/java/com/thomas/readinglist/Reader.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+// comment
 @Entity
 public class Reader implements UserDetails {
 
